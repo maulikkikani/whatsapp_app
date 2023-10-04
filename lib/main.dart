38d2f1e10d0268ad7517daad01799aa6a657a5a9
omdhameliya <omdhameliya54@gmail.com>
2023-10-04 10:45:46 +0530

    whatsapp

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,62 @@
+import 'package:whatsapp_app/helper/helper_function.dart';
+import 'package:whatsapp_app/pages/auth/login_page.dart';
+import 'package:whatsapp_app/pages/home_page.dart';
+import 'package:whatsapp_app/shared/constants.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:whatsapp_app/views/screen/homepage.dart';
-import 'package:whatsapp_app/views/screen/logingpage.dart';
-import 'firebase_options.dart';
+import 'package:flutter/foundation.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(
-    myapp(),
-  );
+
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: FirebaseOptions(
+            apiKey: Constants.apiKey,
+            appId: Constants.appId,
+            messagingSenderId: Constants.messagingSenderId,
+            projectId: Constants.projectId));
+  } else {
+    await Firebase.initializeApp();
+  }
+
+  runApp(const MyApp());
 }
 
-class myapp extends StatelessWidget {
-  const myapp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _isSignedIn = false;
+
+  @override
+  void initState() {
+    super.initState();
+    getUserLoggedInStatus();
+  }
+
+  getUserLoggedInStatus() async {
+    await HelperFunctions.getUserLoggedInStatus().then((value) {
+      if (value != null) {
+        setState(() {
+          _isSignedIn = value;
+        });
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
+    return MaterialApp(
       theme: ThemeData(
-        useMaterial3: true,
-      ),
-      initialRoute: '/logingpage',
-      getPages: [
-        GetPage(
-          name: '/',
-          page: () => homepage(),
-        ),
-        GetPage(
-          name: '/logingpage',
-          page: () => logingpage(),
-        ),
-      ],
+          primaryColor: Constants().primaryColor,
+          scaffoldBackgroundColor: Colors.white),
+      debugShowCheckedModeBanner: false,
+      home: _isSignedIn ? const HomePage() : const LoginPage(),
     );
   }
 }
